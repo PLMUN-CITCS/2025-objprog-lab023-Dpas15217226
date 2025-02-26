e7184691888bafd1129cf409903cda4a93c3fa33
Dpas15217226 <pascualdonnalyn_bsit@plmun.edu.ph>
2025-02-27 07:41:40 +0800

    BankAccountDemo.java

--- BankAccountDemo.java
+++ BankAccountDemo.java
@@ -1,48 +1,48 @@
 // Define the BankAccount class
 class BankAccount {
-    // Attributes with access modifiers
+    // Attributes with different access modifiers
     public String accountHolder;
     private double balance;
     protected String accountType;
-    
-    // Constructor to initialize the attributes
+
+    // Constructor
     public BankAccount(String accountHolder, double balance, String accountType) {
         this.accountHolder = accountHolder;
         this.balance = balance;
         this.accountType = accountType;
     }
-    
-    // Method to deposit money
+
+    // Deposit method
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
         }
     }
-    
-    // Method to withdraw money
+
+    // Withdraw method
     public void withdraw(double amount) {
         if (amount > 0 && amount <= balance) {
             balance -= amount;
         }
     }
-    
-    // Method to get the balance
+
+    // Method to get the current balance
     public double getBalance() {
         return balance;
     }
 }
 
-// Main class to run the program
+// Main class to test the BankAccount functionality
 public class BankAccountDemo {
     public static void main(String[] args) {
         // Create a BankAccount object
-        BankAccount myAccount = new BankAccount("Donna", 10000.0, "Checking");
-        
-        // Perform deposit and withdrawal operations
-        myAccount.deposit(2550.0);
-        myAccount.withdraw(1300.0);
-        
-        // Display account details
+        BankAccount myAccount = new BankAccount("Alice", 1000.0, "Checking");
+
+        // Perform transactions
+        myAccount.deposit(250.0);
+        myAccount.withdraw(100.0);
+
+        // Print account details
         System.out.println("Account Holder: " + myAccount.accountHolder);
         System.out.println("Account Type: " + myAccount.accountType);
         System.out.println("Current Balance: $" + myAccount.getBalance());
